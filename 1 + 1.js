8a161619be3554730bfb6634db7d735f6ae68489
ares1001 <arielgaleppi@gmail.com>
2022-05-09 09:37:35 -0300

    Lunes

---
+++ 1 + 1.js
@@ -0,0 +1,39 @@
+var a = 1,
+    b = 2,
+    c = 3;
+a = b = c;
+
+Math.pow(2, 2) =
+
+
+
+    console.log(a, b, c);
+
+
+function miFuncion() {}
+var otraFuncion = function() {}
+var yOtra = () => {};
+
+
+function logHola(nombre) {
+    console.log('hola,' + nombre);
+}
+
+logHola(Martin);
+
+
+function logHola(nombre) {
+    console.log('Hola, ${nombre');
+
+}
+
+var miNombre = 'Antonio';
+logHola(miNombre);
+
+
+function sumarDosNumeros(a, b) {
+    var suma = a + b;
+    return suma;
+}
+
+sumarDosNumeros(1, 5);
